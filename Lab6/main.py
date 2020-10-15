322017c9ca4f194741376edfbbbb5e3fe1d8f418
Alexey Kirkoskii <kirkovskii.as@phystech,edu>
2020-10-15 12:18:04 +0300

    Balls as a tuple
    Every ball is now represented as a tuple
    Now all balls are in a list of tuples
    Added constant velocity motion
    Added ball destruction on catch

--- Lab6/main.py
+++ Lab6/main.py
@@ -12,20 +12,22 @@ BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 
-def new_ball(surface, x_range=(100, 1100), y_range=(100, 900), radius_range=(10, 100)):
+def new_ball(surface, x_range=(100, 1100), y_range=(100, 900), radius_range=(10, 100), velocity_range=(5, 50)):
     """
-    Draws new ball with random position, color and radius
+    Creates new ball with random position, velocity, radius and color
     :param surface: surface to draw on
     :param x_range: tuple of minimal and maximal x coordinates of the center of the ball
     :param y_range: tuple of minimal and maximal y coordinates of the center of the ball
     :param radius_range: tuple of minimal and maximal ball's radius values
+    :param velocity_range: tuple of maximal and minimal velocity projection on Ox|Oy axis in pixels per second
+    :return: ((x, y, r), (v_x, v_y), color) tuple that contains information about a new ball
     """
-    global x, y, r
     x = randint(*x_range)
     y = randint(*y_range)
     r = randint(*radius_range)
+    v_x, v_y = randint(*velocity_range), randint(*velocity_range)
     color = COLORS[randint(0, 5)]
-    draw.circle(surface, color, (x, y), r)
+    return (x, y, r), (v_x, v_y), color
 
 
 def distance(p1, p2):
@@ -40,26 +42,63 @@ def distance(p1, p2):
     return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 
-def on_mouse_down(surface, event):
+def on_mouse_down(surface, event, balls):
     """
     Mouse button down event handler
     :param surface: main screen of te app
     :param event: a MOUSEBUTTONDOWN event
+    :param balls: list of balls that are on the surface
     """
-    x_mouse, y_mouse = event.pos
-    print("You got it" if (distance((x_mouse, y_mouse), (x, y)) <= r) else "Try again")
+    for i, (pos, vel, color) in enumerate(balls):
+        x, y, r = pos
+        if distance(event.pos, (x, y)) <= r:
+            on_ball_caught(surface, event, balls, i)
 
 
-def draw_frame(screen):
+def on_ball_caught(surface, event, balls, ball_index):
+    """
+    Called when player catches the ball
+    :param balls: list of all balls that are on the screen
+    :param ball_index: index of a ball in balls that has been caught
+    :param surface: surface where the ball was located
+    :param event: MOUSEBUTTONDOWN event of catching click
+    """
+    balls.pop(ball_index)
+
+
+def move_ball(position, velocity, dt):
+    """
+    Moves ball with velocity
+    :param position: tuple (x, y) of ball's coordinates before motion happens
+    :param velocity: (v_x, v_y) tuple that represents velocity vector
+    :param dt: time interval of motion
+    :return: new position of the ball
+    """
+    x, y = position
+    v_x, v_y = velocity
+    return int(x + v_x * dt), int(y + v_y * dt)
+
+
+def draw_frame(surface, balls, fps):
     """
     Draws next frame on screen
-    :param screen: screen to draw on
+    :type fps: FPS
+    :param surface: screen to draw on
+    :param balls: a list of balls that are on the surface
     """
-    pass
+    surface.fill(BLACK)
+    for i, (pos_and_r, vel, color) in enumerate(balls):
+        *pos, r = pos_and_r
+        new_pos_x, new_pos_y = move_ball(pos, vel, 1 / fps)
+        balls[i] = (new_pos_x, new_pos_y, r), vel, color
+        draw.circle(surface, color, (new_pos_x, new_pos_y), r)
 
 
 def main():
     pg.init()
+
+    # tuple ((x, y, r), (v_x, v_y)) represents a ball
+    balls = []
 
     fps = 30
     screen = pg.display.set_mode((1200, 900))
@@ -68,7 +107,7 @@ def main():
     clock = pg.time.Clock()
     finished = False
 
-    new_ball(screen)
+    balls.append(new_ball(screen))
 
     while not finished:
         clock.tick(fps)
@@ -76,10 +115,10 @@ def main():
             if event.type == pg.QUIT:
                 finished = True
             elif event.type == pg.MOUSEBUTTONDOWN:
-                on_mouse_down(screen, event)
+                on_mouse_down(screen, event, balls)
 
-            draw_frame(screen)
-            pg.display.update()
+        draw_frame(screen, balls, fps)
+        pg.display.update()
 
     pg.quit()
 
