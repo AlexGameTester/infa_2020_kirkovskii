7db72e1695d041167c9845da220b71c3d8fa1e36
Alexey Kirkoskii <kirkovskii.as@phystech,edu>
2020-10-21 22:26:16 +0300

    Added new_poly() Renamed random_velocity() to random_vector() and put it into global namespace

--- Lab6/main.py
+++ Lab6/main.py
@@ -18,6 +18,17 @@ FONT = 0
 score = 0
 
 
+def random_vector(magnitude_range):
+    """
+    Return's vector with magnitude from range [m_min, m_max] and random direction
+    :param magnitude_range: tuple (m_min, m_max) of maximal and minimal possible vector magnitudes
+    :return: tuple (v_x, v_y) of vector's coordinates
+    """
+    v = randint(*magnitude_range)
+    a = 2 * pi * random()
+    return v * cos(a), v * sin(a)
+
+
 def new_ball(x_range=(100, 1100), y_range=(100, 900), radius_range=(10, 100), velocity_range=(80, 180)):
     """
     Creates new ball with random position, velocity, radius and color
@@ -28,22 +39,36 @@ def new_ball(x_range=(100, 1100), y_range=(100, 900), radius_range=(10, 100), ve
     :return: ((x, y, r), (v_x, v_y), color) tuple that contains information about a new ball
     """
 
-    def random_velocity(vel_range):
-        """
-        Return's velocity with magnitude from range [v_min, v_max] and random direction
-        :param vel_range: tuple (v_min, v_max) of maximal and minimal possible velocity
-        :return: random velocity from range [-v_max, -v_min]u[v_min, v_max]
-        """
-        v = randint(*vel_range)
-        a = 2 * pi * random()
-        return v * cos(a), v * sin(a)
-
     x = randint(*x_range)
     y = randint(*y_range)
     r = randint(*radius_range)
-    v_x, v_y = random_velocity(velocity_range)
+    v_x, v_y = random_vector(velocity_range)
     color = COLORS[randint(0, 5)]
     return (x, y, r), (v_x, v_y), color
+
+
+def new_poly(vertices_range=(3, 8), x_range=(100, 1100), y_range=(100, 900), radius_range=(10, 100),
+             velocity_range=(80, 180), acceleration_range=(0, 8)):
+    """
+    Creates new poly with random number of vertices, position, velocity, acceleration, radius and color and rotation set to 0
+    :param acceleration_range: tuple of maximal and minimal acceleration projection on Ox|Oy axis in pixels per second squared
+    :param vertices_range: tuple of minimal and maximal numbers of poly's vertices
+    :param x_range: tuple of minimal and maximal x coordinates of the center of the poly
+    :param y_range: tuple of minimal and maximal y coordinates of the center of the poly
+    :param radius_range: tuple of minimal and maximal poly's outer radius values
+    :param velocity_range: tuple of maximal and minimal velocity projection on Ox|Oy axis in pixels per second
+    :return: dictionary that stores poly's properties
+    """
+
+    n = randint(*vertices_range)
+    x = randint(*x_range)
+    y = randint(*y_range)
+    r = randint(*radius_range)
+    vel = random_vector(velocity_range)
+    acc = random_vector(acceleration_range)
+    color = COLORS[randint(0, 5)]
+    return {'vertices': n, 'position': (x, y), 'velocity': vel,
+            'acceleration': acc, 'rotation': 0, 'radius': r, 'color': color}
 
 
 def distance(p1, p2):
